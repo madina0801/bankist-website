df49f75365caf88c568562d1f6836c86e3212d6d
Madina <madinatussupova0820@gmail.com>
2022-09-18 11:15:45 +0600

    menu fade animation

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
-'use strict';
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+"use strict";
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const nav = document.querySelector(".nav");
+
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 ///////////////////////////////////////
 // Modal window
 
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
 
 const openModal = function (e) {
   e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function (e) {
   // e.preventDefault();
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
 
 // Smooth Scrolling
 
-btnScrollTo.addEventListener('click', function(e) {
+btnScrollTo.addEventListener("click", function (e) {
   // Scrolling - Modern way
-  section1.scrollIntoView({behavior: 'smooth'});
+  section1.scrollIntoView({ behavior: "smooth" });
 
   // Old Way
   /*
@@ -57,7 +62,7 @@ btnScrollTo.addEventListener('click', function(e) {
   // console.log('Current scroll(X/Y)', window.pageXOffset, window.pageYOffset);
 
   // console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
-})
+});
 
 ///////////////////////////////////////////////// Page Navigation
 
@@ -65,53 +70,61 @@ btnScrollTo.addEventListener('click', function(e) {
 // 1. Add event listener to common parent elements
 // 2. Determine what element originated the event
 
-document.querySelector('.nav__links').addEventListener('click', function(e) {
+document.querySelector(".nav__links").addEventListener("click", function (e) {
   console.log(e.target);
 
   // Matching strategy
-  if(e.target.classList.contains('nav__link')) {
+  if (e.target.classList.contains("nav__link")) {
     e.preventDefault();
-    const id = e.target.getAttribute('href');
+    const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({
-      behavior: 'smooth'
-    })
+      behavior: "smooth",
+    });
   }
-})
+});
 
 // Tabbed Component - Operations Section
-const tabs = document.querySelectorAll('.operations__tab')
-
-const tabsContainer = document.querySelector('.operations__tab-container')
-
-const tabsContent = document.querySelectorAll('.operations__content')
-
-
-tabsContainer.addEventListener('click', function(e) {
-  const clicked = e.target.closest('.operations__tab');
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
 
   // Remove Active tab and content
   function removeActive(parent, rem) {
-    parent.forEach(e => e.classList.remove(rem))
+    parent.forEach((e) => e.classList.remove(rem));
   }
 
   // Guard Clause
-  if(!clicked) return;
+  if (!clicked) return;
 
   // Active Tab
-  removeActive(tabs, 'operations__tab--active')
-  clicked.classList.add('operations__tab--active');
+  removeActive(tabs, "operations__tab--active");
+  clicked.classList.add("operations__tab--active");
 
   // Active Content Area
-  removeActive(tabsContent, 'operations__content--active');
-  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
-})
+  removeActive(tabsContent, "operations__content--active");
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
 
+// Menu fade animation
+const handleHover = function (o) {
+  return function (e) {
+    if (e.target.classList.contains("nav__link")) {
+      const link = e.target;
+      const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+      const logo = link.closest(".nav").querySelector("img");
 
+      siblings.forEach((el) => {
+        if (el !== link) el.style.opacity = o;
+      });
+      logo.style.opacity = o;
+    }
+  };
+};
 
+nav.addEventListener("mouseover", handleHover(0.5));
 
-
-
-
+nav.addEventListener("mouseout", handleHover(1));
 
 /*
 // Cookie message
@@ -151,13 +164,13 @@ btnScrollTo.addEventListener('click', function(e) {
   });
   */
 
-  // console.log(s1coords);
+// console.log(s1coords);
 
-  // console.log(e.target.getBoundingClientRect());
+// console.log(e.target.getBoundingClientRect());
 
-  // console.log('Current scroll(X/Y)', window.pageXOffset, window.pageYOffset);
+// console.log('Current scroll(X/Y)', window.pageXOffset, window.pageYOffset);
 
-  // console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+// console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
 // })
 
 /*
